b2fe27bccc1a70fba67a34245343db1484f255b2
PedroPaz0156 <n00b56.ppz@gmail.com>
2025-08-07 16:29:41 -0300

    Adicionada a seção de Perguntas Frequentes(FAQ)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() { 
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question');
     
     for (let i = 0; i < buttons.length; i++) { 
         buttons[i].addEventListener('click', function(button) { 
@@ -12,7 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) { 
+        questions[i].addEventListener('click', openOrClosesAnswers);
+    }
 })
+
+function openOrClosesAnswers(element) { 
+    const class1 = 'faq__questions__item--is-open';
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(class1);
+}
 
 function hideAllTabs() { 
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
